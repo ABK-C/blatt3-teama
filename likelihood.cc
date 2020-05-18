9ddd294b08e32f38ba1b1c6dc0e76cf68837d4dc
Anonymous <Anonymous>
2020-05-18 14:38:19 +0000

    Aufgabe 2

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
-
-double poisson(double mu, int k) {
-    return 0;
+double poisson(double mu, int k) 
+{
+  using namespace std;
+  double P = pow(mu, k) * exp(-mu) / tgamma(k + 1);
+  return P;
 }
 
-int main() {
+double prob(std::vector<int> daten, double mu) 
+{
+  using namespace std;
+  long double P = 1;
+  for(int k : daten) 
+  { 
+    P *= poisson(mu , k);
+  }
+   return P;
+}
+
+int main() 
+{
     using namespace std;
-
-
     ifstream fin("datensumme.txt");
+
+    vector<int> daten(0);
+
+
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
     fin.close();
+    cout <<  prob(daten , 3.11538 )<< endl;
 }
